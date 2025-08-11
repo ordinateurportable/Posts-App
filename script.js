fb7dfea79be0b5a13ea4952084aee48df6130762
Stepan Makarov <makarow282@gmail.com>
2025-08-11 10:49:42 +0300

    check if user is in DB

--- script.js
+++ script.js
@@ -4,17 +4,29 @@ const authFormPassword = document.querySelector("#auth-form-password");
 const authFormSubmit = document.querySelector("#auth-form-submit");
 const emailError = document.querySelector("#email-error");
 const passwordError = document.querySelector("#password-error");
-const showPassword = document.querySelector("#show-password");
-const hidePassword = document.querySelector("#hide-password");
+const togglePasswordVisibility = document.querySelector("#toggle-password-visibility");
+const users = [
+    { email: "user1@mail.ru", password: "qwe123QWE"},
+    { email: "user2@mail.ru", password: "asd123ASD"},
+    { email: "user1@mail.ru", password: "zxc123ZXC"},
+];
+
 const validationRules = {
     emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
     passwordRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{4,}$/,
-}
+};
 
 const formValidation = {
     email: false,
     password: false,
-}
+};
+
+const passwordIcons = {
+    eyeOpen:
+      "url(https://api.iconify.design/ic:outline-remove-red-eye.svg?color=%23999999)",
+    eyeClose:
+      "url(https://api.iconify.design/ion:eye-off-outline.svg?color=%23999999)",
+};
 
 checkSubmitDisbaled = () => {
     if (formValidation.email && formValidation.password)
@@ -25,13 +37,18 @@ checkSubmitDisbaled = () => {
         }
 }
 
-authForm.addEventListener('submit', (e) => {
+authForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const formObj = new formData(e.target)
     const formData = Object.fromEntries(formObj);
     if(formData.email.match(validationRules.emailRegex) && 
     formData.password.match(validationRules.passwordRegex)) {
-        console.log("yes");
+        const isUser = users.find(
+            (user) =>
+              formData.email === user.email && formData.password === user.password
+          );
+      
+          console.log(isUser);
     }
 })
 
@@ -51,6 +68,12 @@ authFormEmail.addEventListener("input", (e) => {
 );
 
 authFormPassword.addEventListener("input", (e) => {
+    if (e.target.value) {
+        togglePasswordVisibility.classList.remove("hidden");
+    } else {
+        togglePasswordVisibility.classList.add("hidden");
+    }
+    
     if(e.target.value.match(validationRules.passwordRegex)) {
         formValidation.password = true;
         passwordError.classList.add("invisible");
@@ -63,8 +86,19 @@ authFormPassword.addEventListener("input", (e) => {
 
 });
 
-showPassword.addEventListener("change", () => {
-    authFormPassword.type = "text";
-    showPassword.classList.add("hidden");
-    hidePassword.classList.remove("hidden");
-  });
+
+togglePasswordVisibility.addEventListener("click", (e) => {
+    if (e.target.dataset.visibility === "true") {
+      //hide
+      authFormPassword.type = "password";
+      e.target.style.backgroundImage = passwordIcons.eyeOpen;
+      e.target.dataset.visibility = "false";
+    } else {
+      //show
+      authFormPassword.type = "text";
+      e.target.style.backgroundImage = passwordIcons.eyeClose;
+      e.target.dataset.visibility = "true";
+    }
+  
+    authFormPassword.focus();
+});
